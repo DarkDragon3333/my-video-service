483edacc0817a355a1611ad0434651a8a5d65760
DarkDragon3333 <minaichiev-dragon2017@yandex.ru>
2026-06-09 17:23:09 +0300

    feat: add response status 201 for user creation

--- src/main/java/com/kino/my_video_service/controller/UserController.java
+++ src/main/java/com/kino/my_video_service/controller/UserController.java
@@ -4,10 +4,8 @@ import com.kino.my_video_service.dto.CreateUserRequest;
 import com.kino.my_video_service.dto.UserResponse;
 import com.kino.my_video_service.entities.UserEntity;
 import com.kino.my_video_service.service.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -20,6 +18,7 @@ public class UserController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public UserResponse createUser(@RequestBody CreateUserRequest request) {
 
         UserEntity tempEntity = userService.createUser(request.getLogin(), request.getDisplayName(), request.getPassword());
