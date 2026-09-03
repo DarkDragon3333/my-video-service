ba84eb7350d035fd9292ad7bf75a354640fa1be4
DarkDragon3333 <minaichiev-dragon2017@yandex.ru>
2026-09-03 14:55:08 +0300

    refactor: extract movie response creation logic into private method

--- src/main/java/com/kino/my_video_service/controller/MovieController.java
+++ src/main/java/com/kino/my_video_service/controller/MovieController.java
@@ -31,108 +31,71 @@ public class MovieController {
                 createMovieRequest.getDurationMinutes()
         );
 
-        return new MovieResponse(
-                movieEntity.getId(), movieEntity.getTitle(), movieEntity.getRequiredPlan(),
-                movieEntity.getGenre(), movieEntity.getDescription(), movieEntity.getReleaseDate(),
-                durationToMinutes(movieEntity.getDuration()), movieEntity.getRating()
-        );
+        return toResponse(movieEntity);
     }
 
     @GetMapping("/{id}")
     public MovieResponse getMovie(@PathVariable Long id){
         MovieEntity movieEntity = movieService.findMovieById(id);
-
-        return new MovieResponse(
-                movieEntity.getId(), movieEntity.getTitle(), movieEntity.getRequiredPlan(),
-                movieEntity.getGenre(), movieEntity.getDescription(), movieEntity.getReleaseDate(),
-                durationToMinutes(movieEntity.getDuration()), movieEntity.getRating()
-        );
+        return toResponse(movieEntity);
     }
 
     @GetMapping
     public List<MovieResponse> getAll(){
         List<MovieEntity> movieEntityList = movieService.findAll();
-
         return movieEntityList.stream().map(
-                movie ->
-                        new MovieResponse(
-                                movie.getId(), movie.getTitle(), movie.getRequiredPlan(),
-                                movie.getGenre(), movie.getDescription(), movie.getReleaseDate(),
-                                durationToMinutes(movie.getDuration()), movie.getRating()
-                        )
+                this::toResponse
         ).toList();
     }
 
     @PatchMapping("/{id}/title")
     public MovieResponse patchTitle(@PathVariable Long id, @RequestBody @Valid ChangeTitleRequest changeTitleRequest){
         MovieEntity movieEntity = movieService.patchTitle(id, changeTitleRequest.getTitle());
-
-        return new MovieResponse(
-                movieEntity.getId(), movieEntity.getTitle(), movieEntity.getRequiredPlan(),
-                movieEntity.getGenre(), movieEntity.getDescription(), movieEntity.getReleaseDate(),
-                durationToMinutes(movieEntity.getDuration()), movieEntity.getRating()
-        );
+        return toResponse(movieEntity);
     }
 
     @PatchMapping("/{id}/description")
     public MovieResponse patchDescription(@PathVariable Long id, @RequestBody @Valid ChangeDescriptionRequest description){
         MovieEntity movieEntity = movieService.patchDescription(id, description.getDescription());
-
-        return new MovieResponse(
-                movieEntity.getId(), movieEntity.getTitle(), movieEntity.getRequiredPlan(),
-                movieEntity.getGenre(), movieEntity.getDescription(), movieEntity.getReleaseDate(),
-                durationToMinutes(movieEntity.getDuration()), movieEntity.getRating()
-        );
+        return toResponse(movieEntity);
     }
 
     @PatchMapping("/{id}/required-plan")
     public MovieResponse patchRequiredPlan(@PathVariable Long id, @RequestBody @Valid ChangeRequiredPlanRequest requiredPlanRequest){
         MovieEntity movieEntity = movieService.patchRequiredPlan(id, requiredPlanRequest.getRequiredPlan());
-
-        return new MovieResponse(
-                movieEntity.getId(), movieEntity.getTitle(), movieEntity.getRequiredPlan(),
-                movieEntity.getGenre(), movieEntity.getDescription(), movieEntity.getReleaseDate(),
-                durationToMinutes(movieEntity.getDuration()), movieEntity.getRating()
-        );
+        return toResponse(movieEntity);
     }
 
     @PatchMapping("/{id}/genre")
     public MovieResponse patchGenre(@PathVariable Long id, @RequestBody @Valid ChangeGenreRequest genreRequest){
         MovieEntity movieEntity = movieService.patchGenre(id, genreRequest.getGenre());
-
-        return new MovieResponse(
-                movieEntity.getId(), movieEntity.getTitle(), movieEntity.getRequiredPlan(),
-                movieEntity.getGenre(), movieEntity.getDescription(), movieEntity.getReleaseDate(),
-                durationToMinutes(movieEntity.getDuration()), movieEntity.getRating()
-        );
+        return toResponse(movieEntity);
     }
 
     @PatchMapping("/{id}/release-date")
     public MovieResponse patchReleaseDate(@PathVariable Long id, @RequestBody @Valid ChangeReleaseDateRequest releaseDateRequest){
         MovieEntity movieEntity = movieService.patchReleaseDate(id, releaseDateRequest.getReleaseDate());
-
-        return new MovieResponse(
-                movieEntity.getId(), movieEntity.getTitle(), movieEntity.getRequiredPlan(),
-                movieEntity.getGenre(), movieEntity.getDescription(), movieEntity.getReleaseDate(),
-                durationToMinutes(movieEntity.getDuration()), movieEntity.getRating()
-        );
+        return toResponse(movieEntity);
     }
 
     @PatchMapping("/{id}/duration")
     public MovieResponse patchDuration(@PathVariable Long id, @RequestBody @Valid ChangeDurationRequest changeDurationRequest){
         MovieEntity movieEntity = movieService.patchDuration(id, changeDurationRequest.getDurationMinutes());
-
-        return new MovieResponse(
-                movieEntity.getId(), movieEntity.getTitle(), movieEntity.getRequiredPlan(),
-                movieEntity.getGenre(), movieEntity.getDescription(), movieEntity.getReleaseDate(),
-                durationToMinutes(movieEntity.getDuration()), movieEntity.getRating()
-        );
+        return toResponse(movieEntity);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteMovie(@PathVariable Long id){
         movieService.deleteMovie(id);
+    }
+    
+    private MovieResponse toResponse(MovieEntity movieEntity) {
+        return new MovieResponse(
+                movieEntity.getId(), movieEntity.getTitle(), movieEntity.getRequiredPlan(),
+                movieEntity.getGenre(), movieEntity.getDescription(), movieEntity.getReleaseDate(),
+                durationToMinutes(movieEntity.getDuration()), movieEntity.getRating()
+        );
     }
 
     private Integer durationToMinutes(Duration duration){
